8da041e8fc6c2794617eee4cec99a0205e6b3879
Bhoumick Sudan <bomix18@gmail.com>
2025-10-13 14:41:51 +0530

    Update variable.R

--- variable.R
+++ variable.R
@@ -1,10 +1,11 @@
 #Creating Variables in R
-name <- "John"
-age <- 40
+name <- "Bomix"
+age <- 20
 
 name # auto-print the value of the name variable
 age # auto-print the value of the age variable
 
 #Using print function
-name <- "John Doe"
+name <- "Bomix"
+
 print(name) # print the value of the name variable 
